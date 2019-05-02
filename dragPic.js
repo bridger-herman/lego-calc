a27c20e61dc1371cb0aa547dad8ff92b2b37863b
Bridger Herman <herma582@umn.edu>
2019-05-01 21:07:09 -0500

    Append empty div for refrigerator

--- dragPic.js
+++ dragPic.js
@@ -13,17 +13,17 @@ function setup() {
       $(dropped).detach().css({top: 0, left: 0}).appendTo($(droppedOn).find('.appliance-drop'));
       $(dropped).draggable('disable');
 
-      if (!$(dropped).hasClass('noinput')) {
-        let minMaxValStep = [0, 14, 1];
-        let defaultText = '0 Times/Week';
-        if ($(dropped).hasClass('percentage')) {
-          minMaxValStep = [0, 1, 0.1];
-          defaultText = 'Running 0% time';
-        } else if ($(dropped).hasClass('hourly')) {
-          minMaxValStep = [0, 24, 2];
-          defaultText = '0 Hours/Day';
-        }
+      let minMaxValStep = [0, 14, 1];
+      let defaultText = '0 Times/Week';
+      if ($(dropped).hasClass('percentage')) {
+        minMaxValStep = [0, 1, 0.1];
+        defaultText = 'Running 0% time';
+      } else if ($(dropped).hasClass('hourly')) {
+        minMaxValStep = [0, 24, 2];
+        defaultText = '0 Hours/Day';
+      }
 
+      if (!$(dropped).hasClass('noinput')) {
         $(droppedOn).find('.appliance-input').append(
             $('<div/>')
             .append($('<input/>', {
@@ -40,21 +40,25 @@ function setup() {
               text: defaultText,
             }))
         );
-
-        $('.slider').on('input', (evt) => {
-          let val = $(evt.target).val();
-          let fstr = `${val} Times/Week`;
-          if ($(dropped).hasClass('percentage')) {
-            fstr = `Running ${val * 100}% time`;
-          } else if ($(dropped).hasClass('hourly')) {
-            fstr = `${val} Hours/Day`;
-          }
-          $(evt.target).parent().find('.value-output').html(fstr);
-          let usage = calculateEnergyUsage();
-          plotLegos(calculateNumLegos(usage));
-          $('#usage-kwh').html(`${totalEnergyUsage(usage)} kWh`);
-        });
+      } else {
+        $(droppedOn).find('.appliance-input').append(
+          $('<div/>', { css: { width: '110px' } })
+        );
       }
+
+      $('.slider').on('input', (evt) => {
+        let val = $(evt.target).val();
+        let fstr = `${val} Times/Week`;
+        if ($(dropped).hasClass('percentage')) {
+          fstr = `Running ${val * 100}% time`;
+        } else if ($(dropped).hasClass('hourly')) {
+          fstr = `${val} Hours/Day`;
+        }
+        $(evt.target).parent().find('.value-output').html(fstr);
+        let usage = calculateEnergyUsage();
+        plotLegos(calculateNumLegos(usage));
+        $('#usage-kwh').html(`${totalEnergyUsage(usage)} kWh`);
+      });
 
       let usage = calculateEnergyUsage();
       plotLegos(calculateNumLegos(usage));
